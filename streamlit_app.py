22886f927d757d3fddbbc6c931037f43dd0859d6
wxlim26 <85481998+wxlim26@users.noreply.github.com>
2024-04-09 00:00:47 +0800

    test commit
    test commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,23 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas_gbq as pd_gbq
 
-"""
-# Welcome to Streamlit!
+# # Create API client.
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"]
+# )
+# client = bigquery.Client(credentials=credentials)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# directory to privatekey.json
+cred = "secrets/privatekey.json"
+credentials = service_account.Credentials.from_service_account_file(f"{cred}")
+client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+project_id = credentials.project_id
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Perform query.
+query = """SELECT * FROM bt4301.ttao.salaries LIMIT 10"""
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+salary_df = pd_gbq.read_gbq(query, project_id= project_id)
+st.dataframe(salary_df)
