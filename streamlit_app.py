f27034c643fe92a118145a44b97f26936f951ee4
wxlim26 <85481998+wxlim26@users.noreply.github.com>
2024-04-09 00:05:06 +0800

    commit2
    gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,10 @@ from google.cloud import bigquery
 import pandas_gbq as pd_gbq
 
 # # Create API client.
-# credentials = service_account.Credentials.from_service_account_info(
-#     st.secrets["gcp_service_account"]
-# )
-# client = bigquery.Client(credentials=credentials)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
 
-
-# directory to privatekey.json
-cred = "secrets/privatekey.json"
-credentials = service_account.Credentials.from_service_account_file(f"{cred}")
 client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 project_id = credentials.project_id
 
