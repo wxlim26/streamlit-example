3b98a5fb6275daa311aea4d40f5a99bf410f7631
wxlim26 <85481998+wxlim26@users.noreply.github.com>
2024-04-09 23:57:16 +0800

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
-import pandas_gbq as pd_gbq
+# import pandas_gbq as pd_gbq
+import json
 
 # # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
@@ -24,14 +25,19 @@ st.caption('test')
 @st.cache_data(ttl=600)
 def run_query(query):
     query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
+    df = query_job.to_dataframe()
+    return df
+# def run_query(query):
+#     query_job = client.query(query)
+#     rows_raw = query_job.result()
+#     # Convert to list of dicts. Required for st.cache_data to hash the return value.
+#     rows = [dict(row) for row in rows_raw]
+#     return rows
 
-rows = run_query("""SELECT * FROM bt4301.ttao.salaries LIMIT 10""")
 
-# Print results.
-st.write("Some wise words from Shakespeare:")
-for row in rows:
-    st.write(row)
+df = run_query("""SELECT * FROM bt4301.ttao.salaries LIMIT 10""")
+st.dataframe(df)
+# # Print results.
+# st.write("Some wise words from Shakespeare:")
+# for row in rows:
+#     st.write(row)
