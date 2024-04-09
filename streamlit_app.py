0bed5aece3832f1f5cd655267dfee5d5c051dbbd
wxlim26 <85481998+wxlim26@users.noreply.github.com>
2024-04-09 23:36:59 +0800

    Update streamlit_app.py
    proj id

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 
-# project_id = credentials.project_id
-# client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+project_id = credentials.project_id
+client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
-client = bigquery.Client(credentials=credentials)
+st.write(project_id)
 st.caption('test')
 # Perform query.
 query = """SELECT * FROM bt4301.ttao.salaries LIMIT 10"""
 
-salary_df = pd_gbq.read_gbq(query)
+salary_df = pd_gbq.read_gbq(query, project_id= project_id)
 st.dataframe(salary_df)
